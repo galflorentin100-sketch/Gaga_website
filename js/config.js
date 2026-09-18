6ef987e9948775f5fe906b90683000145fe767f1
galflorentin100-sketch <galflorentin100@gmail.com>
2026-09-19 00:07:28 +0300

    Update Supabase anon key in config.js

--- js/config.js
+++ js/config.js
@@ -5,4 +5,4 @@
 // הדביקו כאן:
 // ============================================================
 const SUPABASE_URL = "https://ogvlugkkmenzsjfrqefe.supabase.co/rest/v1/";
-const SUPABASE_ANON_KEY = "TZ1kHjzb";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9ndmx1Z2trbWVuenNqZnJxZWZlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODk3MjE0ODIsImV4cCI6MjEwNTI5NzQ4Mn0.CHy5pb9r8dOMx01xsBAts-JQAi_ugYd2fjO0FWSHFjU";
