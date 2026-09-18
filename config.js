bcc5bfa835b07c18c26fba7410e7ef49916f1b9d
galflorentin100-sketch <galflorentin100@gmail.com>
2026-09-18 14:04:14 +0300

    Update config.js

--- config.js
+++ config.js
@@ -4,5 +4,5 @@
 // Project Settings -> API -> Project URL + anon public key
 // הדביקו כאן:
 // ============================================================
-const SUPABASE_URL = "https://YOUR-PROJECT-ID.supabase.co";
-const SUPABASE_ANON_KEY = "YOUR-ANON-PUBLIC-KEY";
+const SUPABASE_URL = "https://ogvlugkkmenzsjfrqefe.supabase.co/rest/v1/";
+const SUPABASE_ANON_KEY = "TZ1kHjzb";
