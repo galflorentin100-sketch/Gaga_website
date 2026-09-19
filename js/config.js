19ed9da1f8ff112787c829a4b064baeb0947fe82
galflorentin100-sketch <galflorentin100@gmail.com>
2026-09-19 10:20:13 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -5,5 +5,6 @@
 // הדביקו כאן:
 // ============================================================
 const SUPABASE_URL = "https://ogvlugkkmenzsjfrqefe.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9ndmx1Z2trbWVuenNqZnJxZWZlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODk3MjE0ODE0ODIsImV4cCI6MjEwNTI5NzQ4Mn0.CHy5pb9r8dOMx01xsBAtsJQAi_ugYd2fjO0FWSHFjU";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9ndmx1Z2trbWVuenNqZnJxZWZlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODk3MjE0ODIsImV4cCI6MjEwNTI5NzQ4Mn0.CHy5pb9r8dOMx01xsBAts-JQAi_ugYd2fjO0FWSHFjU";
+
 
